ba17c13710edec2c1a9368b975d3b12c53afc5e5
tjlee0821 <teo0821@gmail.com>
2022-04-23 08:22:08 +0900

    step-1

---
+++ Quiz/quiz.py
@@ -0,0 +1,17 @@
+WIDTH = 1280
+HEIGHT = 720
+
+def draw():
+    print('def draw')
+    
+def game_over():
+    print('def game_over')
+    
+def correct_answer():
+    print('def correct_answer')
+    
+def on_mouse_down(pos):
+    print('def on_mouse_down')
+    
+def update_time_left():
+    print('def')
