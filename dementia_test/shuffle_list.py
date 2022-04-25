934e4c9f6d73faa662b30c48517cc186649d1017
tjlee0821 <teo0821@gmail.com>
2022-04-26 00:19:15 +0900

    step-11

--- dementia_test/shuffle_list.py
+++ dementia_test/shuffle_list.py
@@ -11,6 +11,8 @@ screenSize = "1280x1024"
 
 target_list = []
 
+keys = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
+
 reptiles = ['adder', 'alligator', 'anaconda', 'angonoka', 'anole', 'asp', 'basilisk', 'black caiman', 'black racer', 'boaconstrictor', 'box turtle', 'bull snake', 'caiman', 'cobra', 'collared lizard', 'copperhead', 'cottonmouth', 'crocodile', 'desert tortoise', 'frilled lizard', 'gaboon viper', 'garter snake', 'gavial', 'gecko', 'gila monster', 'glass lizard', 'gopher snake', 'green iguana', 'ground skink', 'horned lizard', 'iguana', 'indigo snake']
 
 mammals = ['Bat', 'Bear', 'Beaver', 'Cat', 'Cow', 'Coyote', 'Deer', 'Dog', 'Dolphin', 'Elephant', 'Fox', 'Gibbon', 'Giraffe', 'Goat', 'Goat', 'Gopher', 'Hedgehog', 'Hippopotamus', 'Horse', 'Horse', 'Jaguar', 'Kangaroo', 'Koala', 'Leopard', 'Lion', 'Llama', 'Lynx', 'Mole', 'Monkey', 'Mouse', 'Narwhal', 'Orangutan', 'Orca', 'Otter', 'Ox', 'Panda', 'Pig', 'Polar bear', 'Puma', 'Rabbit', 'Raccoon', 'Rat', 'Rhinoceros', 'Sheep', 'Squirrel', 'Tiger', 'Walrus', 'Weasel', 'Wolf', 'Zebra']
@@ -54,13 +56,23 @@ def BuildQuestionAndAnswerList(itx):
     answer_List.append(wrong_List)
     random.shuffle(answer_List)
     
-    return right_List, answer_List
-
-def ShowQuestionAndAnswer(itx): #잘 만들어졌는지 확인해본다
-    (right_List,answer_List) = BuildQuestionAndAnswerList(itx)
+    right_List_dict = dict()
+    answer_List_dict = dict()
+    right_List_dict = dict(zip(keys,right_List))
+    answer_List_dict = dict(zip(keys,answer_List))
     
-    print("Question :",right_List)
-    print("Answer :",answer_List)
+    return right_List_dict, answer_List_dict
+
+def showQuestionAndAnswer(itx): #잘 만들어졌는지 확인해본다
+    right_List_dict = dict()
+    answer_List_dict = dict()
+    (right_List_dict,answer_List_dict) = BuildQuestionAndAnswerList(itx)
+    
+    print("Question :",right_List_dict)
+    print("Answer :",answer_List_dict)
+    
+    qestion_1 = right_List_dict.get(1)
+    print(qestion_1)        
     
 
 ###=============================###
@@ -75,18 +87,15 @@ def ChangeSpecies():
 
 ###============================###
 
-def print_Question(qests,clrs):
-    qLb.config(text=qests)
-    qLb.config(bg=clrs)
+def print_Question(qests,clrs, lgc):
+#    qLb.config(text=qests)
+#    qLb.config(bg=clrs)
     print(qests)
     print(clrs)
+    print(lgc)
     
 ###============================###
-def showQuestion(itx):
-    (right_List,answer_List) = BuildQuestionAndAnswerList(itx)
-    for sName, sColor, sLogic in right_List:
-        print_Question(sName, sColor)   
-        time.sleep(1)
+
         
 
 ###============================###
@@ -94,10 +103,10 @@ def testStart():
     ChangeSpecies()
     print(target_list)
     print("Start")
-    showQuestion(6)# 버튼 배치
+    showQuestionAndAnswer(6)
+    # 버튼 배치
+    #root.after(10, update_status)
     
-
-
 ###============================###
 
 root = tk.Tk()
@@ -126,11 +135,30 @@ btn.config(width=20)
 btn.config(command=testStart)      
 btn.pack()            
 
-qLb = Label(root)
-qLb.config(height=50)
-qLb.config(width=720)
-qLb.config(bg='blue')
-qLb.pack()            
+#qLb = Label(root)
+#qLb.config(height=50)
+#qLb.config(width=720)
+#qLb.config(bg='blue')
+#qLb.pack()            
+
+
+status = tk.Label(root, text="Working")
+status.pack()
+
+def update_status():
+    # Get the current message
+    current_status = status["text"]
+    # If the message is "Working...", start over with "Working"
+    if current_status.endswith("..."): current_status = "Working"
+    # If not, then just add a "." on the end
+    else: current_status += "."
+    # Update the message
+    status.config(text = current_status)
+    # After 1 second, update the status
+    root.after(1000, update_status)
+# Launch the status message after 1 millisecond (when the window is loaded)
+#root.after(1, update_status)
+
 
 root.mainloop()
     
