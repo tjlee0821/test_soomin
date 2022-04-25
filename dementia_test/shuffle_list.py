6ffc65cb2c89b553f117666eab27acee83120c1a
tjlee0821 <teo0821@gmail.com>
2022-04-25 13:19:19 +0900

    step-7

--- dementia_test/shuffle_list.py
+++ dementia_test/shuffle_list.py
@@ -1,6 +1,11 @@
 import random
+import tkinter as tk
+from tkinter import *
 
+global changer
 changer = False
+screenSize = "1280x1024"
+
 
 target_list = []
 
@@ -10,39 +15,84 @@ mammals = ['Bat', 'Bear', 'Beaver', 'Cat', 'Cow', 'Coyote', 'Deer', 'Dog', 'Dolp
 
 colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Purple']
 
-#random.shuffle(mammals)
-
-if changer == True: 
-    target_list.extend(mammals)
-if changer == False: 
-    target_list.extend(reptiles)    
-    
-print(target_list)    
-    
-list_length = len(target_list)
-list_length_colors = len(colors)
+list_length_mammals = len(mammals)
+list_length_reptiles = len(reptiles)
 
 questionLength = 5
-
-rRightList = random.sample(target_list,questionLength)
-rRightColors = random.sample(colors,questionLength)
-rRightLogic = []
-for i in range(0,questionLength):
-    rRightLogic.append(True)
-
-rWrongList = random.sample(target_list,1)
-rWrongColors = random.sample(colors,1)
-rWrongLogic = [False]
-
-right_List = list(zip(rRightList,rRightColors,rRightLogic))
-wrong_List = list(zip(rWrongList,rWrongColors,rWrongLogic))
-
-answer_list = random.sample(right_List,3)
-answer_list.append(wrong_List)
-random.shuffle(answer_list)
+MaxQuestionLength = min(list_length_mammals, list_length_reptiles)
 
 
-print(right_List)
-print(answer_list)
+def setTargetList(str):
+    if str == True: 
+        target_list.extend(mammals)
+        print(target_list)
+    if str == False: 
+        target_list.extend(reptiles)    
+        list_length = len(target_list)
+        list_length_colors = len(colors)
+        print(target_list)
+    print("^^^")
+
+def BuildQuestionAndAnswerList(itx):
+    rRightList = random.sample(target_list,itx)
+    for i in range(0,itx): # to make enough color element for target_list 이게 없으면 애러난다. itx 가 컬러 수보다 커지기 때문이다.
+        colors.extend(colors)
+        
+    rRightColors = random.sample(colors,itx)
+    rRightLogic = []
+    for i in range(0,itx):#수만큼 true 를 넣어준다.
+        rRightLogic.append(True)
+
+    rWrongList = random.sample(target_list,1)
+    rWrongColors = random.sample(colors,1)
+    rWrongLogic = [False]
+
+    right_List = list(zip(rRightList,rRightColors,rRightLogic))
+    wrong_List = list(zip(rWrongList,rWrongColors,rWrongLogic))
+
+    answer_List = random.sample(right_List,3)
+    answer_List.append(wrong_List)
+    random.shuffle(answer_List)
+    
+    return right_List, answer_List
+
+def ShowQuestionAndAnswer(itx): #잘 만들어졌는지 확인해본다
+    (right_List,answer_List) = BuildQuestionAndAnswerList(itx)
+    
+    print("Question :",right_List)
+    print("Answer :",answer_List)
+    
+
+###=============================###
+
+def DarkenLabel():
+    if chkvar1.get() == 1:
+        label.config(bg="gray")
+        setTargetList(True)
+    if chkvar1.get() == 0:
+        label.config(bg="white")
+        setTargetList(False)
+    
+
+root = tk.Tk()
+root.title("dementia_test")   
+root.wm_geometry(screenSize)
+
+label = Label(root, bg="white", pady=5, font=(None, 1), height=20, width=720)
+chkvar1 = IntVar()
+checkbox = Checkbutton(root, bg="white", command=DarkenLabel, variable=chkvar1)
+checkbox.config(text="Mammals")
+checkbox.grid(row=0, column=0, sticky="w")
+label.grid(row=0, column=0, sticky="ew")
+#chkvar1 = IntVar()                            # chkvar1에 int 형으로 값을 저장
+#chkbox1 = Checkbutton(root, variable=chkvar1, command=DarkenLabel) # root라는 창에 체크박스 생성
+#chkbox1.config(text="Mammals")                      # 체크박스 내용 설정
+#chkbox1.pack()
+
+
+root.mainloop()
+    
+
+
 
 
